bad2fcc83c685081e96f7b839b99729f3a39ca28
KoenKwakkenbos <k.kwakkenbos@gmail.com>
2024-04-07 13:18:20 +0200

    Implements modes for classification and defocus augmentation

--- src/dataset/datagenerator.py
+++ src/dataset/datagenerator.py
@@ -40,6 +40,7 @@ class DataGenerator(keras.utils.Sequence):
             A.RandomBrightnessContrast(brightness_limit=(-0.2, 0.2), contrast_limit=(-0.15, 0.15), p=0.75),
             # A.RandomGamma(p=0.5),
             A.GaussNoise(var_limit=(0.0, 200.0), p=0.75),
+            A.Defocus(radius=(1, 3), p=0.75)
         ])
         self.on_epoch_end()
 
@@ -99,15 +100,14 @@ class DataGenerator(keras.utils.Sequence):
 
 class ClassificationDataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
-    def __init__(self, list_IDs, img_path, label_df, batch_size=32, dim=(800,800), n_channels=1,
-                 n_classes=1, shuffle=True, augmentation=True):
+    def __init__(self, list_IDs, img_path, label_df, batch_size=32, dim=(256,256), n_channels=1,
+                 n_classes=1, shuffle=True, augmentation=True, mask_path=None, mode=1):
         'Initialization'
-        self.dim = dim
+        self.dim = (*dim, n_channels)
         self.batch_size = batch_size
         self.list_IDs = list_IDs
         self.img_path = img_path
         self.labels = label_df
-        self.n_channels = n_channels
         self.n_classes = n_classes
         self.shuffle = shuffle
         self.augmentation = augmentation
@@ -120,7 +120,10 @@ class ClassificationDataGenerator(keras.utils.Sequence):
             A.RandomBrightnessContrast(brightness_limit=(-0.2, 0.2), contrast_limit=(-0.15, 0.15), p=0.75),
             # A.RandomGamma(p=0.5),
             A.GaussNoise(var_limit=(0, 200), p=0.75),
+            A.Defocus(radius=(1, 3), p=1)
         ])
+        self.mode = mode
+        self.mask_path = mask_path
         self.on_epoch_end()
 
     def __len__(self):
@@ -130,7 +133,7 @@ class ClassificationDataGenerator(keras.utils.Sequence):
     def __getitem__(self, index):
         'Generate one batch of data'
         # Generate indexes of the batch
-        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]\
 
         # Find list of IDs
         list_IDs_temp = [self.list_IDs[k] for k in indexes]
@@ -151,6 +154,21 @@ class ClassificationDataGenerator(keras.utils.Sequence):
         img = skimage.io.imread(path, as_gray=True)
 
         return img
+    
+    def crop_around_mask(self, image, mask):
+        """Crops the image around the non-zero regions of the mask."""
+        rows, cols = np.where(mask > 0)
+        ymin, ymax = rows.min(), rows.max() + 1
+        xmin, xmax = cols.min(), cols.max() + 1
+        cropped_image = image[ymin:ymax, xmin:xmax]
+        return cropped_image
+
+    # def pad_to_original_size(self, image, original_shape):
+    #     """Pads the image with zeros to match the original shape."""
+    #     missing_rows = original_shape[0] - image.shape[0]
+    #     missing_cols = original_shape[1] - image.shape[1]
+    #     padded_image = np.pad(image, ((0, missing_rows), (0, missing_cols), (0, 0)), mode='constant')
+    #     return padded_image
 
 
     def __data_generation(self, list_IDs_temp):
@@ -159,22 +177,79 @@ class ClassificationDataGenerator(keras.utils.Sequence):
         X = np.empty((self.batch_size, *self.dim), dtype=np.uint8)
         y = np.empty(self.batch_size, dtype=np.uint8)
 
-        # Generate data
-        for i, ID in enumerate(list_IDs_temp):
+        if self.mode != 1:
+            X_mask = np.empty((self.batch_size, *self.dim), dtype=np.uint8)
 
-            # Read image and mask
-            img = self.read_image(os.path.join(self.img_path, str(int(ID)) + '.jpg'))
-            X[i,] = np.stack((img,)*3, axis=-1)
-            y[i] = self.labels.loc[ID, 'outcome']
+        if self.mode == 1:
+            # Generate data
+            for i, ID in enumerate(list_IDs_temp):
 
-            if self.augmentation:
-                augmented = self.transform(image=X[i,])
-                X[i,] = augmented['image']
+                # Read image and mask
+                img = self.read_image(os.path.join(self.img_path, str(int(ID)) + '.jpg'))
+                # X[i,] = np.stack((img,)*3, axis=-1)
+                X[i,] = np.expand_dims(img, -1)
+                y[i] = self.labels.loc[ID, 'outcome']
 
-        # X = np.expand_dims(X, axis=3)
-        # y = np.expand_dims(y, axis=3).astype(bool)
+                if self.augmentation:
+                    augmented = self.transform(image=X[i,])
+                    X[i,] = augmented['image']
 
-        return X, y
+            
+            return X, y
+            
+        elif self.mode == 2:
+            # Generate data
+            for i, ID in enumerate(list_IDs_temp):
+
+                # Read image and mask
+                img = self.read_image(os.path.join(self.img_path, str(int(ID)) + '.jpg'))
+                mask = self.read_image(os.path.join(self.mask_path, str(int(ID)) + '_mask.tif'))
+                # X[i,] = np.stack((img,)*3, axis=-1)
+                X[i,] = np.expand_dims(img, -1)
+                X_mask[i] = np.expand_dims(mask, -1)
+
+                y[i] = self.labels.loc[ID, 'outcome']
+
+                if self.augmentation:
+                    augmented = self.transform(image=X[i,], mask=X_mask[i])
+                    X[i,] = augmented['image']
+                    X_mask[i,] = augmented['mask']
+
+                # ELSE!
+
+            plt.imshow(X_mask[0,], cmap='gray')
+            plt.show()
+
+            return [X, X_mask], y
+        
+        elif self.mode == 3:
+            # Generate data
+            for i, ID in enumerate(list_IDs_temp):
+
+                # Read image and mask
+                img = self.read_image(os.path.join(self.img_path, str(int(ID)) + '.jpg'))
+                mask = self.read_image(os.path.join(self.mask_path, str(int(ID)) + '_mask.tif'))
+
+                y[i] = self.labels.loc[ID, 'outcome']
+
+                if self.augmentation:
+                    augmented = self.transform(image=img, mask=mask)
+                    # X[i,] = np.expand_dims(augmented['image'] * augmented['mask'], -1)
+
+                        # Crop around the mask
+                    img_cropped = self.crop_around_mask(augmented['image'] * augmented['mask'], augmented['mask'])
+                    
+                    # pad 10 pixels around the cropped image
+                    img_cropped = cv2.copyMakeBorder(img_cropped, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=0)
+
+                    # Pad to original size if necessary
+                    img_cropped = cv2.resize(img_cropped, (self.dim[0], self.dim[1]))
+
+                    X[i,] = np.expand_dims(img_cropped, -1)
+            
+                # ELSE!
+
+            return X, y
     
 
 if __name__ == "__main__":
@@ -186,7 +261,7 @@ if __name__ == "__main__":
 
     df_label['label'] = df_label['outcome']
 
-    datagen = ClassificationDataGenerator(list_IDs=df_label.index, img_path=IMG_PATH, label_df=df_label, batch_size=8, dim=(800,800, 3), n_channels=1)
+    datagen = ClassificationDataGenerator(list_IDs=df_label.index, img_path=IMG_PATH, label_df=df_label, batch_size=8, dim=(800, 800), n_channels=1, mode=3, mask_path=IMG_PATH+'masks/')
 
     X, y = datagen.__getitem__(0)
     
